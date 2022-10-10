a6a74b45f49b367f21d2e1e7d4c8596a0bcfd14b
TPlemur <TPlemur@gmail.com>
2022-10-10 16:59:36 -0700

    Added random bubbles

--- game.js
+++ game.js
@@ -26,9 +26,14 @@ l  l
 `
 ];
 
+
+
 const G ={
    WIDTH: 150,
    HEIGHT: 150, 
+   NUMBUB: 20,
+   BUBMIN: 0.5,
+   BUBMAX: 1.0
 };
 
 options = {
@@ -36,10 +41,36 @@ options = {
     theme: "dark"
   };
 
+/** 
+ * @typedef {object} Bubble
+ * @property {Vector} pos
+ * @property {number} speed
+*/
+/**
+ * @type {Bubble[]}
+ */
+let bubbles;
+
 function update() {
     if (!ticks) {
+      //make bubbles with random pos and speed
+      bubbles = times(G.NUMBUB, ()=>{
+        return{
+          pos: vec(rnd(0, G.WIDTH),rnd(0,G.HEIGHT)),
+          speed: rnd(G.BUBMIN,G.BUBMAX)
+        };
+      });
+
     }
     char('a',75,75)
+    // Update bubbles
+    color("light_black");
+    bubbles.forEach((s) => {
+      s.pos.x -= s.speed;
+      s.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+      box(s.pos, 1);
+    });
+    color("black");
 }
 
 addEventListener("load", onLoad);
