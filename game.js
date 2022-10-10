f79bf276be264a8a8f72d7560df0006c2c692c14
rbm717 <47369758+rbm717@users.noreply.github.com>
2022-10-10 16:46:10 -0700

    Player swimming

--- game.js
+++ game.js
@@ -31,6 +31,12 @@ const G ={
    HEIGHT: 150, 
 };
 
+let player = {
+  pos: vec(G.WIDTH * 0.25, G.HEIGHT * 0.75)
+};;
+
+let swimSpd = 1;
+
 options = {
     viewSize: {x: G.WIDTH, y:G.HEIGHT},
     theme: "dark"
@@ -40,6 +46,14 @@ function update() {
     if (!ticks) {
     }
     char('a',75,75)
+    char('b', player.pos);
+    player.pos.clamp(0, G.WIDTH, 10, G.HEIGHT - 10); //Keeps player from going offscreen
+    // Player "swimming" controls
+    if(input.isPressed){
+      player.pos.y -= swimSpd;
+    }else{
+      player.pos.y += swimSpd;
+    }
 }
 
 addEventListener("load", onLoad);
